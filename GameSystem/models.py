65e560a102ba6ea2ae3eabcbbc1d87c73b6a6f8e
Alex Williams <cs.alexwilliams@gmail.com>
2015-11-08 05:54:47 -0500

    Added initial models.

--- GameSystem/models.py
+++ GameSystem/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Round(models.Model):
+    result = models.IntegerField(default=-1)  # 0 == loss, 1 == win
+    subject = models.IntegerField(default=-1)  # id of the subject (image)
+    user1_tags = models.TextField(default="", blank=True)
+    user2_tags = models.TextField(default="", blank=True)
+    time = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Game(models.Model):
+    user1 = models.IntegerField(default=-1)
+    user2 = models.IntegerField(default=-1)
+    rounds = models.ManyToManyField(Round)
+    score = models.IntegerField(default=-1)
+
+
+class Subject(models.Model):
+    url = models.TextField(default="", blank=True)
