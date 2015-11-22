390fed9ee29a73271ed7e24a7e75c48ed3ab0870
William Callaghan <wcallag3@uwo.ca>
2015-11-22 15:27:44 -0500

    generate 2 lists of songs with some percentage of identity

--- GameSystem/views.py
+++ GameSystem/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from GameSystem.models import *
+import random
 
 
 def home(request):
@@ -61,15 +62,54 @@ def load_game_session(request, availablegame_id):
 
     # get pre-existing game with this user
 
-    # TODO:
+
     # 1. Create a list of subjects for each user with some percentage of identity.
     #   e.g.    user1: [4, 5, 3, 10, 15, 1, 9, 8, 2, 11]
     #           user2: [3, 5, 4, 1, 9, 11, 2, 8, 10, 15]
     # 2. Serve the subject id at the relevant index.
 
+    NUM_CLIPS = 20
+    NUM_ROUNDS = 10
+    NUM_SAME = 4
+
+    clips = range(1, NUM_CLIPS)
+    user1 = random.sample(range(1, NUM_CLIPS), 10)
+    user2 = random.sample(range(1, NUM_CLIPS), 10)
+
+    # Generate two list where user1[index] != user2[index]
+    for index in range(len(user2)):
+        if (user1[index] == user2[index]):
+            # Figure out what song we can replace in user2
+            # so they do not listen to the same song twice
+            canAdd = list(set(clips) - set(user2))
+            # Grab a random song from this list
+            replace = random.choice(canAdd)
+            # Replace it in the user2
+            user2[index] = replace
+
+
+    # Create a list of random indexes (determining where
+    # to serve the same clips to both)
+    sameIndex = random.sample(range(0, NUM_ROUNDS-1), NUM_SAME)
+
+    for index in range(len(sameIndex)):
+        item1 = user1[sameIndex[index]]
+        item2 = user2[sameIndex[index]]
+        if item1 not in user2:
+            user2[sameIndex[index]] = item1
+        elif item2 not in user1:
+            user1[sameIndex[index]] = item2
+        else:
+            # Generate a single list of random IDs not currently
+            # in both lists
+            randomID = list(set(clips) - set(user1) - set(user2))
+            item = random.choice(randomID)
+            user1[sameIndex[index]] = item
+            user2[sameIndex[index]] = item
+
+
     # TODO: (in the form submit view)
     # 1. Increment the game's index
     # 2. Save the tags.
-
 
     return render(request, 'game_interface.html', {'game': game})
