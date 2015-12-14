a98d154ed753eb9d77ca9eb734c4c5403ae44062
Alex Williams <cs.alexwilliams@gmail.com>
2015-12-14 16:27:06 -0500

    Added manage.py command.

---
+++ GameSystem/management/commands/extract.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from GameSystem.models import *
+from django.contrib.auth.models import User
+import os
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Used to dump CSV data.'
+
+    def _extract_db(self):
+        print ' :: Dumping CSV Format Data ::'
+
+        games = Game.objects.all()
+
+        for game in games:
+            user1 = User.objects.get(id=game.user1)
+            user2 = User.objects.get(id=game.user1)
+
+            print " -- Game "+str(game.id)+" --"
+            for round in game.rounds.all():
+                print str(user1.username)+','+str(user2.username)+','+str(round.user1_subject)+","+str(round.user2_subject)+","+str(round.user1_tags)+","+str(round.user2_tags)
+            print
+
+        print ' :: Data successfully dumped :: (lol)'
+
+    def handle(self, *args, **options):
+        self._extract_db()
